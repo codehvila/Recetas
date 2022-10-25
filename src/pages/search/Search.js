7814c4d47528d1fb06b65b870881ee565a7fa0be
Hugo Vila <code.hvila@gmail.com>
2022-10-25 18:31:48 +0200

    Create the result Search page

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,5 +1,25 @@
+import { useSearchParams } from "react-router-dom";
+
 import styles from "./Search.module.css";
 
+import useFetch from "../../hooks/useFetch";
+
+import RecipeList from "../../components/RecipeList";
+
 export default function Search() {
-  return <div className={styles.Search}>Search</div>;
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("q");
+  const url = "http://localhost:3005/recipes?q=" + query;
+
+  const { data: recipes, isPending, error } = useFetch(url);
+
+  return (
+    <div className={styles.Search}>
+      <h2>Search</h2>
+      <p>Recipes including '{searchParams.get("q")}'</p>
+      {error && <p className={styles.error}>{error}</p>}
+      {isPending && <p className={styles.spinner}>Loadding Recipes...</p>}
+      {recipes && <RecipeList data={recipes} />}
+    </div>
+  );
 }
