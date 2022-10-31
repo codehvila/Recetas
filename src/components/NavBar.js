fecf3113c10a03af51b691ef2d7c27334397903b
Hugo Vila <code.hvila@gmail.com>
2022-10-31 18:45:23 +0100

    Use the reducer to manage the NavBar background color

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,10 +7,22 @@ import { useTheme } from "../hooks/useTheme";
 import styles from "./NavBar.module.css";
 
 export default function NavBar() {
-  const { color } = useTheme();
+  const { color, changeBgColorTo } = useTheme();
+
+  const switchBgColor = () => {
+    if (color === "#333") {
+      changeBgColorTo("rgb(89, 4, 146)");
+    } else {
+      changeBgColorTo("#333");
+    }
+  };
 
   return (
-    <div className={styles.navbar} style={{ backgroundColor: color }}>
+    <div
+      onClick={switchBgColor}
+      className={styles.navbar}
+      style={{ backgroundColor: color }}
+    >
       <nav>
         <Link to="/" className={styles.brand}>
           Grandmother's Recipes
