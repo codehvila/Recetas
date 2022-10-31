3d5b00015f7804d007a5205b89bfd9fdf22dc368
Hugo Vila <code.hvila@gmail.com>
2022-10-31 18:43:39 +0100

    Setup useReducer to better control state

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,10 +1,28 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const ThemeContext = createContext();
 
+const ThemeReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_BG_COLOR":
+      return { ...state, color: action.payload };
+
+    default:
+      return state;
+  }
+};
+
 export default function ThemeProvider({ children }) {
+  const [state, dispatch] = useReducer(ThemeReducer, {
+    color: "rgb(89, 4, 146)",
+  });
+
+  const changeBgColorTo = (color) => {
+    dispatch({ type: "CHANGE_BG_COLOR", payload: color });
+  };
+
   return (
-    <ThemeContext.Provider value={{ color: "#333" }}>
+    <ThemeContext.Provider value={{ ...state, changeBgColorTo }}>
       {children}
     </ThemeContext.Provider>
   );
