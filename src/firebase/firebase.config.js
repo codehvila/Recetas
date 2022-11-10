460cc062607cc42d889b200f131a856559435a08
Hugo Vila <code.hvila@gmail.com>
2022-11-10 18:55:56 +0100

    Configure firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const db = firebase.firestore();
+
+export { db };
