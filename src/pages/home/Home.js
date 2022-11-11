62a28e724cd4ce3b5422af8ccf1c3a3aa5315172
Hugo Vila <code.hvila@gmail.com>
2022-11-11 18:47:10 +0100

    Connect Recipe List to get recipes from firebase db

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,20 +1,44 @@
+import { useEffect, useState } from "react";
 import RecipeList from "../../components/RecipeList";
-import useFetch from "../../hooks/useFetch";
+
+import { db } from "../../firebase/firebase.config";
 
 import styles from "./Home.module.css";
 
 export default function Home() {
-  const {
-    data: recipes,
-    isPending,
-    error,
-  } = useFetch("http://localhost:3005/recipes");
+  const [recipes, setRecipes] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setIsPending(true);
+
+    db.collection("recipes")
+      .get()
+      .then((querySnapshot) => {
+        if (querySnapshot.empty) {
+          setError("No recipes to show!");
+          setIsPending(false);
+        } else {
+          let results = [];
+          querySnapshot.docs.forEach((doc) => {
+            results.push({ id: doc.id, ...doc.data() });
+            setRecipes(results);
+            setIsPending(false);
+          });
+        }
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsPending(false);
+      });
+  }, []);
 
   return (
     <div className={styles.Home}>
       <h2>Home Recipes</h2>
       <div>
-        {error && <p className={styles.error}>{error}</p>}
+        {error && <p className="error">{error}</p>}
         {isPending && <p className={styles.spinner}>Loadding Recipes...</p>}
         {recipes && <RecipeList data={recipes} />}
       </div>
